6b55a2f715f123f82419630770201b500a68f799
yohanstra <jreinstra@gmail.com>
2017-09-28 11:10:17 -0700

    add request (wont work yet)

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ $(".secondFade").delay(1750).animate({ opacity: 1 }, 1500);
 $(".thirdFade").delay(2000).animate({ opacity: 1 }, 1500);
 
 $("#submitEmail").click(function() {
-    $("#emailForm").html("<br>Thank you. We'll keep you posted.");
+    $.post(
+        "http://localhost:8000/",
+        {"email": $("#emailInput").val()},
+        function(resp) {
+            console.log(resp);
+            $("#emailForm").html("<br>Thank you. We'll keep you posted.");
+        },
+    );
 });
